034a3859fb1afba416304954b8d50f7ce6a20809
xiong <3124725835@qq.com>
2023-05-17 21:24:49 +0800

    微微改动

--- test/main.cpp
+++ test/main.cpp
@@ -1,20 +1,28 @@
 #include<iostream>
-#include"main.h"
+#include<vector>
+#include<string>
 using namespace std;
 
 int main() {
-	int num_10[10] = { 2 };
-	int(*pnum[10][10][10][10])[10][10][10][10];
-	cout << sizeof(pnum) << endl;
-	cout << sizeof(*pnum) << endl;
-	cout << sizeof(**pnum) << endl;
-	cout << sizeof(***pnum) << endl;
-	cout << sizeof(****pnum) << endl;
-	cout << sizeof(*****pnum) << endl;
-	cout << sizeof(******pnum) << endl;
-	cout << sizeof(*******pnum) << endl;
-	cout << sizeof(********pnum) << endl;
-	cout << sizeof(*********pnum) << endl;
+	vector<string>v;
+	v.push_back("123");
+	v.push_back("1234");
+	v.push_back("12345");
+
+	for (int i = 0; i < v.size(); i++) {
+		cout << v[i] << endl;
+	}
+	// 向指定位置插入元素
+	v.insert(v.begin(), "123456");
+	for (int i = 0; i < v.size(); i++) {
+		cout << v[i] << endl;
+	}
+	cout << "*************" << endl;
+	int n[10] = { 0 };
+	for (auto elem : n) {
+		cout << elem << endl;
+	}
+
 	return 0;
 }
 
